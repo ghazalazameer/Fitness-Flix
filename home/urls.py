f19453d1b3ea3de15ead0fade95b625a1bb64f13
Ghazala Zameer <134362001+ghazalazameer@users.noreply.github.com>
2023-11-19 01:44:35 +0530

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from .views import protected_view, CustomLoginView
+
+urlpatterns = [
+    path("", views.hero, name='home'),
+    path("hero", views.hero, name='hero'),
+    path("about", views.about, name='about'),
+    path("signup", views.signup_views, name='signup'),
+    path("contact", views.contact, name='contact'), 
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path('protected', protected_view, name='protected'),
+]
+  
+   
